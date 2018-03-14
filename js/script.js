6482d619c1d3e8365dcd4b00f39d2cf1032e1b5d
morten-pedersen <mortenpedersennn1989@gmail.com>
2018-03-14 15:07:29 +0100

    la til json array i script.js

--- js/script.js
+++ js/script.js
@@ -1,222 +1,248 @@
-function initMap() {
-  var Bergen = {lat: 60.39299, lng: 5.32415};
-  var Landmark = {lat: 60.389704, lng: 5.326552};
-  var Hulen = {lat: 60.384759, lng: 5.325364};
-  var Kvarteret = {lat: 60.389701, lng: 5.322013};
-  var Garage = {lat: 60.389432, lng: 5.323846};
-  var Østre = {lat: 60.393748, lng: 5.328094};
-  var Koengen = {lat: 60.400002, lng: 5.318541};
-  var Verftet = {lat: 60.394722, lng: 5.310569};
-
-
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,
-    center: Bergen,
-    styles: [
-{
-"elementType": "geometry",
-"stylers": [
+var toaletter = [
   {
-    "color": "#f5f5f5"
-  }
-]
-},
-{
-"elementType": "labels.icon",
-"stylers": [
-  {
-    "visibility": "off"
-  }
-]
-},
-{
-"elementType": "labels.text.fill",
-"stylers": [
-  {
-    "color": "#616161"
-  }
-]
-},
-{
-"elementType": "labels.text.stroke",
-"stylers": [
-  {
-    "color": "#f5f5f5"
-  }
-]
-},
-{
-"featureType": "administrative.land_parcel",
-"elementType": "labels.text.fill",
-"stylers": [
-  {
-    "color": "#bdbdbd"
-  }
-]
-},
-{
-"featureType": "poi",
-"elementType": "geometry",
-"stylers": [
-  {
-    "color": "#eeeeee"
-  }
-]
-},
-{
-"featureType": "poi",
-"elementType": "labels.text.fill",
-"stylers": [
-  {
-    "color": "#757575"
-  }
-]
-},
-{
-"featureType": "poi.park",
-"elementType": "geometry",
-"stylers": [
-  {
-    "color": "#e5e5e5"
-  }
-]
-},
-{
-"featureType": "poi.park",
-"elementType": "labels.text.fill",
-"stylers": [
-  {
-    "color": "#9e9e9e"
-  }
-]
-},
-{
-"featureType": "road",
-"elementType": "geometry",
-"stylers": [
-  {
-    "color": "#ffffff"
-  }
-]
-},
-{
-"featureType": "road.arterial",
-"elementType": "labels.text.fill",
-"stylers": [
-  {
-    "color": "#757575"
-  }
-]
-},
-{
-"featureType": "road.highway",
-"elementType": "geometry",
-"stylers": [
-  {
-    "color": "#dadada"
-  }
-]
-},
-{
-"featureType": "road.highway",
-"elementType": "labels.text.fill",
-"stylers": [
-  {
-    "color": "#616161"
-  }
-]
-},
-{
-"featureType": "road.local",
-"elementType": "labels.text.fill",
-"stylers": [
-  {
-    "color": "#9e9e9e"
-  }
-]
-},
-{
-"featureType": "transit.line",
-"elementType": "geometry",
-"stylers": [
-  {
-    "color": "#e5e5e5"
-  }
-]
-},
-{
-"featureType": "transit.station",
-"elementType": "geometry",
-"stylers": [
-  {
-    "color": "#eeeeee"
-  }
-]
-},
-{
-"featureType": "water",
-"elementType": "geometry",
-"stylers": [
-  {
-    "color": "#c9c9c9"
-  }
-]
-},
-{
-"featureType": "water",
-"elementType": "labels.text.fill",
-"stylers": [
-  {
-    "color": "#9e9e9e"
-  }
-]
+    "entries": [{
+        "herre": "1",
+        "tid_sondag": "07.00 - 23.15",
+        "pissoir_only": "NULL",
+        "stellerom": "NULL",
+        "latitude": "60.388076",
+        "tid_hverdag": "07.00 - 23.15",
+        "plassering": "NONNESETER TERMINAL, SØR",
+        "tid_lordag": "07.00 - 23.15",
+        "rullestol": "1",
+        "adresse": "Lungegårdskaien",
+        "pris": "12",
+        "id": "1",
+        "place": "NONNESETER TERMINAL, SOUTH",
+        "dame": "1",
+        "longitude": "5.334937"
+    }, {
+        "herre": "1",
+        "tid_sondag": "NULL",
+        "pissoir_only": "NULL",
+        "stellerom": "NULL",
+        "latitude": "60.388871",
+        "tid_hverdag": "05.30 - 23.50",
+        "plassering": "NONNESETER TERMINAL , NORD",
+        "tid_lordag": "07.00 - 23.15",
+        "rullestol": "1",
+        "adresse": "Østre Strømkai",
+        "pris": "12",
+        "id": "2",
+        "place": "NONNESETER TERMINAL , NORTH",
+        "dame": "1",
+        "longitude": "5.334079"
+    }, {
+        "herre": "1",
+        "tid_sondag": "NULL",
+        "pissoir_only": "NULL",
+        "stellerom": "NULL",
+        "latitude": "60.388868",
+        "tid_hverdag": "09.00 - 17.00",
+        "plassering": "SKYSS KUNDESENTER",
+        "tid_lordag": "09.00 - 15.00",
+        "rullestol": "1",
+        "adresse": "Østre Strømkai",
+        "pris": "12",
+        "id": "3",
+        "place": "SKYSS CUSTOMER CENTRE",
+        "dame": "1",
+        "longitude": "5.334079"
+    }, {
+        "herre": "1",
+        "tid_sondag": "07.00 - 23.00",
+        "pissoir_only": "NULL",
+        "stellerom": "NULL",
+        "latitude": "60.39041",
+        "tid_hverdag": "07.00 - 23.00",
+        "plassering": "JERNBANESTASJONEN",
+        "tid_lordag": "07.00 - 23.00",
+        "rullestol": "NULL",
+        "adresse": "Strømgaten 4",
+        "pris": "10",
+        "id": "4",
+        "place": "RAILWAY STATION",
+        "dame": "1",
+        "longitude": "5.332995"
+    }, {
+        "herre": "1",
+        "tid_sondag": "08.30 - 22.00",
+        "pissoir_only": "NULL",
+        "stellerom": "1",
+        "latitude": "60.394554",
+        "tid_hverdag": "09.00 - 23.00",
+        "plassering": "MATHALLEN",
+        "tid_lordag": "08.30 - 22.00",
+        "rullestol": "1",
+        "adresse": "Strandkaien 3",
+        "pris": "10",
+        "id": "5",
+        "place": "FISH MARKET",
+        "dame": "1",
+        "longitude": "5.324099"
+    }, {
+        "herre": "1",
+        "tid_sondag": "08.00 - 18.00",
+        "pissoir_only": "NULL",
+        "stellerom": "",
+        "latitude": "60.395432",
+        "tid_hverdag": "08.00 - 18.00",
+        "plassering": "STRANDKAITERMINALEN",
+        "tid_lordag": "08.00 - 18.00",
+        "rullestol": "",
+        "adresse": "Strandkaien",
+        "pris": "10",
+        "id": "6",
+        "place": "STRANDKAI BOAT TERMINAL",
+        "dame": "1",
+        "longitude": "5.321034"
+    }, {
+        "herre": "1",
+        "tid_sondag": "NULL",
+        "pissoir_only": "NULL",
+        "stellerom": "NULL",
+        "latitude": "60.3913929",
+        "tid_hverdag": "08.00 - 15.00",
+        "plassering": "BERGEN KOMMUNE, INNBYGGERSERVICE",
+        "tid_lordag": "NULL",
+        "rullestol": "1",
+        "adresse": "Kaigaten 4",
+        "pris": "0",
+        "id": "7",
+        "place": "CITIZEN SERVICE CENTRE",
+        "dame": "1",
+        "longitude": "5.326855"
+    }, {
+        "herre": "1",
+        "tid_sondag": "NULL",
+        "pissoir_only": "NULL",
+        "stellerom": "1",
+        "latitude": "60.3891794",
+        "tid_hverdag": "09.00 - 21.00",
+        "plassering": "BERGEN STORSENTER",
+        "tid_lordag": "09.00 - 18.00",
+        "rullestol": "1",
+        "adresse": "Strømgaten 8",
+        "pris": "10",
+        "id": "8",
+        "place": "BERGEN STORSENTER",
+        "dame": "1",
+        "longitude": "5.3305793"
+    }, {
+        "herre": "1",
+        "tid_sondag": "NULL",
+        "pissoir_only": "NULL",
+        "stellerom": "1",
+        "latitude": "60.392209",
+        "tid_hverdag": "09.00 - 21.00",
+        "plassering": "SUNDT MOTEHUS",
+        "tid_lordag": "09.00 - 18.00",
+        "rullestol": "1",
+        "adresse": "Torgallmenningen 14",
+        "pris": "10",
+        "id": "9",
+        "place": "SUNDT FASHION HOUSE",
+        "dame": "1",
+        "longitude": "5.324011"
+    }, {
+        "herre": "1",
+        "tid_sondag": "NULL",
+        "pissoir_only": "NULL",
+        "stellerom": "1",
+        "latitude": "60.3928444",
+        "tid_hverdag": "09.00 - 20.00",
+        "plassering": "XHIBITION",
+        "tid_lordag": "09.00 - 18.00",
+        "rullestol": "1",
+        "adresse": "Småstrandgaten 3",
+        "pris": "10",
+        "id": "10",
+        "place": "XHIBITION",
+        "dame": "1",
+        "longitude": "5.3239541"
+    }, {
+        "herre": "1",
+        "tid_sondag": "NULL",
+        "pissoir_only": "NULL",
+        "stellerom": "1",
+        "latitude": "60.3947371",
+        "tid_hverdag": "09.00 - 21.00",
+        "plassering": "GALLERIET",
+        "tid_lordag": "09.00 - 18.00",
+        "rullestol": "1",
+        "adresse": "Torgallmenningen 8",
+        "pris": "10",
+        "id": "11",
+        "place": "GALLERIET",
+        "dame": "1",
+        "longitude": "5.3243561"
+    }, {
+        "herre": "1",
+        "tid_sondag": "NULL",
+        "pissoir_only": "NULL",
+        "stellerom": "1",
+        "latitude": "60.3944194",
+        "tid_hverdag": "10.00 - 20.00",
+        "plassering": "KLØVERHUSET",
+        "tid_lordag": "10.00 - 18.00",
+        "rullestol": "1",
+        "adresse": "Strandgaten 13 -15",
+        "pris": "10",
+        "id": "12",
+        "place": "KLØVERHUSET",
+        "dame": "1",
+        "longitude": "5.3205649"
+    }, {
+        "herre": "1",
+        "tid_sondag": "09.00 - 18.00",
+        "pissoir_only": "NULL",
+        "stellerom": "NULL",
+        "latitude": "60.3974108",
+        "tid_hverdag": "09.00 - 18.00",
+        "plassering": "BRYGGEN BESØKSSENTER",
+        "tid_lordag": "09.00 - 18.00",
+        "rullestol": "1",
+        "adresse": "Jacobsfjorden, Bryggen",
+        "pris": "10",
+        "id": "13",
+        "place": "BRYGGEN VISITOR CENTRE",
+        "dame": "1",
+        "longitude": "5.3222145"
+    }, {
+        "herre": "NULL",
+        "tid_sondag": "ALL",
+        "pissoir_only": "1",
+        "stellerom": "NULL",
+        "latitude": "60.397359",
+        "tid_hverdag": "ALL",
+        "plassering": "C. SUNDTSGT",
+        "tid_lordag": "ALL",
+        "rullestol": "NULL",
+        "adresse": "C. Sundts gt",
+        "pris": "NULL",
+        "id": "14",
+        "place": "C. SUNDTSGT",
+        "dame": "NULL",
+        "longitude": "5.313963"
+    }, {
+        "herre": "NULL",
+        "tid_sondag": "ALL",
+        "pissoir_only": "1",
+        "stellerom": "NULL",
+        "latitude": "60.397557",
+        "tid_hverdag": "ALL",
+        "plassering": "NORDNES SKOLE",
+        "tid_lordag": "ALL",
+        "rullestol": "NULL",
+        "adresse": "Nordnesparken 3",
+        "pris": "NULL",
+        "id": "15",
+        "place": "NORDNES SKOLE",
+        "dame": "NULL",
+        "longitude": "5.307858"
+    }],
+    "page": 1,
+    "pages": 1,
+    "posts": 15
 }
 ]
-  });
-
-  var marker = new google.maps.Marker({
-    position: Landmark,
-    map: map,
-    title: 'Landmark'
-  });
-
-  var marker = new google.maps.Marker({
-    position: Hulen,
-    map: map,
-    title: 'Hulen'
-  });
-
-  var marker = new google.maps.Marker({
-    position: Kvarteret,
-    map: map,
-    title: 'Kvarteret'
-  });
-
-  var marker = new google.maps.Marker({
-    position: Garage,
-    map: map,
-    title: 'Garage'
-  });
-
-  var marker = new google.maps.Marker({
-    position: Koengen,
-    map: map,
-    title: 'Koengen'
-  });
-
-  var marker = new google.maps.Marker({
-    position: Verftet,
-    map: map,
-    title: 'Verftet'
-  });
-
-  var marker = new google.maps.Marker({
-    position: Østre,
-    map: map,
-    title: 'Østre'
-  });
-
-
-}
-
-async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCBuVLxQGcWI91xujnPjh1NsfEDLK6X-jU&callback=initMap"
