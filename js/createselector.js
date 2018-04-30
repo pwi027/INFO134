7462f64cd8eb759187664d6e83e5d796b0e53960
pwi027 <Paal>
2018-04-30 22:40:00 +0200

    Oppgave 8
    Opprettet createselector.js

---
+++ js/createselector.js
@@ -0,0 +1,44 @@
+/**
+* Lager og returnerer et HTML-element.
+* Legger til attributes hvis de er oppgitt.
+*
+* @param {string} tag tagnavnet, f.eks. 'select', 'p' eller 'div'.
+* @param {object} [attributes] et objekt som inneholder attributter som skal
+*                              legges til på elementet, f.eks. id: 'myID'.
+* @returns {HTMLElement} et HTML-element
+*/
+function create(tag, attributes) {
+  var element = document.createElement(tag);
+
+  if (typeof attributes === 'object') {
+    for (var key in attributes) {
+      if (attributes.hasOwnProperty(key)) {
+        element[key] = attributes[key];
+      }
+    }
+  }
+
+  return element;
+}
+
+/**
+ * Funksjonen lager et select-element med valgalternativer
+ * fra parameteret, og returnerer select-elementet.
+ *
+ * @param {object[]} options valgalternativer
+ */
+function createSelector(options) {
+  var selector = create('select');
+
+  for (var option in options) {
+    var option = create('option', {
+      value: option.id
+    });
+
+    option.innerHTML = option.name;
+
+    selector.appendChild(option);
+  }
+
+  return selector;
+}
